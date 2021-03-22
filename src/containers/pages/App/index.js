38ad718d72c303e0214a3057a2a4bd3265e66c39
purwokang <purwo.com@gmail.com>
2021-03-22 13:38:31 +0700

    setup redux pada project

--- src/containers/pages/App/index.js
+++ src/containers/pages/App/index.js
@@ -1,19 +1,22 @@
-import logo from "../../../assets/img/logo/logo.svg";
 import "./App.css";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import Dashboard from "../Dashboard";
 import Login from "../Login";
 import Register from "../Register";
+import { Provider } from "react-redux";
+import { store } from "../../../config/redux";
 
 function App() {
   return (
-    <Router>
-      <div>
-        <Route path="/" exact component={Dashboard} />
-        <Route path="/login" component={Login} />
-        <Route path="/register" component={Register} />
-      </div>
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <div>
+          <Route path="/" exact component={Dashboard} />
+          <Route path="/login" component={Login} />
+          <Route path="/register" component={Register} />
+        </div>
+      </Router>
+    </Provider>
   );
 }
 
